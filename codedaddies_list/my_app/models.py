750c411df3eb99c6a7db4b5b7a52691f2c53af13
alex <alexruadev@gmail.com>
2020-02-22 22:54:17 +0700

    Added front end and requirements

--- codedaddies_list/my_app/models.py
+++ codedaddies_list/my_app/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class Search(models.Model):
+    # in our model called Search, create one field named search, all search takes in an input
+    search = models.CharField(max_length=500)
+    # timestamp field (created_at)
+    created = models.DateTimeField(auto_now=True)
+
+    # To return our search with what was typed instead of an object, in our database
+    def __str__(self):
+        return '{}'.format(self.search)
+
+    # To put search field plural in our database
+    class Meta:
+        verbose_name_plural = 'Searches'
